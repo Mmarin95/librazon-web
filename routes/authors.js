d827f17c111afb413f9f327249313972c17777c2
Miquel Marin <mmarin_79@hotmail.com>
2021-02-17 21:03:58 +0100

    add create authors

---
+++ routes/authors.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+
+const Author = require("../models/author");
+
+// Author index route
+router.get("/", (req, res) => {
+  res.render("authors/index");
+});
+
+// New author route
+router.get("/new", (req, res) => {
+  res.render("authors/new", { author: new Author() });
+});
+
+// Create author route
+router.post("/", async (req, res) => {
+  const author = new Author({
+    name: req.body.name,
+  });
+
+  try {
+    const newAuthor = await author.save();
+    // res.redirect(`authors/${newAuthor.id}`);
+    res.redirect("authors");
+  } catch {
+    res.render("authors/new", {
+      author: author,
+      errorMessage: "Error creating author",
+    });
+  }
+});
+
+module.exports = router;
